e6ee5d74c5c64ec77e1ad1679171d5341c086661
gogularaja <gogularaja.sr@cloudenablers.com>
2017-03-22 16:55:04 +0530

    Update app.py

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    provider = str(os.environ.get('PROVIDER', 'world version 3.0'))
+    provider = str(os.environ.get('PROVIDER', 'world version 4.0'))
     return 'Hello  '+provider+'!'
 
 if __name__ == '__main__':
